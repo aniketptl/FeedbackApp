10a63869351f9d1d3a54aa02d5373de7a014bd1c
Aniket Patil <aniketp563@gmail.com>
2016-10-15 13:49:17 +0530

    Custom back button to settings view controller

---
+++ ViewSettingsController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewSettingsController.swift
+//  FeedbackApp
+//
+//  Created by Aniket Patil on 15/10/16.
+//  Copyright © 2016 Aniket Patil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewSettingsController: UIViewController {
+    
+    @IBOutlet weak var genericList: UITextField!
+    @IBOutlet weak var dateList: UITextField!
+    @IBOutlet weak var testomonialCloudLink: UITextField!
+    @IBOutlet weak var linkingDetailsPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Custom Back Button
+        self.navigationItem.hidesBackButton = true
+        let newBackButton = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.Plain, target: self, action: #selector(ViewSettingsController.back(_:)))
+        self.navigationItem.leftBarButtonItem = newBackButton;
+        //End Custom Back Button
+    }
+    
+    func back(sender: UIBarButtonItem) {
+        //self.performSegueWithIdentifier("unwindToViewController", sender: self)
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+}
